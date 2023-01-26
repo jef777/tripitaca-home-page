3b290337a141dd28ac8e1c0325579f44e01bb2d0
jeff <jeffmuigai11@gmail.com>
2023-01-26 09:55:39 +0300

    Add: Bootstrap and base css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import App from './App.vue'
 import Vuex from 'vuex'
 import store from './store'
 
+//Bootstrap 5 resources
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+//custom style sheet
+import './style.css'
+
 const app = createApp(App)
 app.use(Vuex)
 app.use(store)
